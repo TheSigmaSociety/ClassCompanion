0748b51bc6bb57c3f0540cdaafd61d167dc113e6
gamedoescool <hi_ishan@outlook.com>
2024-04-20 22:09:44 -0700

    commit1

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
-import InputParser from './inputParser.js';
-const inputParser = new InputParser();
+const recognition = new webkitSpeechRecognition() || new SpeechRecognition();
+recognition.interimResults = true;
+recognition.continuous = true;
+const startButton = document.getElementById('startButton');
+let start1 = true;
+g = [];
+function start() {
+  if(start1) { 
+    recognition.start();
+  }
+  else {
+    recognition.stop();
+  }
+  start1 = !start1;
+}
 
-inputParser.listen();
-setTimeout(() => {
-  inputParser.stop();
-}, 20000);
+recognition.onresult = event => {
+    const result = event.results[event.results.length - 1];
+    if(result.isFinal) {
+      g.push(result[0].transcript); 
+      console.log(g);
+    }
+};
+
+recognition.onend = () => {
+  g = [];
+  console.log('finished')
+};
+
+recognition.onerror = event => {
+    console.error('Speech recognition error:', event.error);
+};
+
+recognition.onnomatch = () => {
+    console.log('No speech was recognized.');
+};
+
 
 
