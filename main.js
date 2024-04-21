6d4aaea067d3ee61d35c8eddc84b32cfd09b98dd
Hugo H <huangfeiyang2008@gmail.com>
2024-04-21 14:22:43 -0700

    Co-authored-by: SanixWheee <SanixWheee@users.noreply.github.com> Co-authored-by: gamedoescool <gamedoescool@users.noreply.github.com>

--- main.js
+++ main.js
@@ -66,11 +66,13 @@ class Queue {
   async response() {
     const completion = await this.openai.chat.completions.create({
       messages: [
-        { role: "system", content: "summarize and provide detailed notes from the following sentences from a live speech transcript. correct for any unintended mistakes. each input from the transcript is around 10 sentences long, separated by /" },
-        { role: "user", content:  this.elements.join("/")}],
+        { role: "system", content: "summarize and provide one sentence of notes for each input from the transcript. Make sure they accurately reflect the topic of the notes, as students will be using these in order to study, but also make sure they are not too long as the point is to summarize it concicely for the students use. correct for any unintended mistakes. each input from the transcript is around 10 sentences long. If you are unable to access the transcript, do not return anything." },
+        { role: "user", content:  this.elements.join("")}],
       model: "gpt-3.5-turbo-0125",
     });
     console.log(completion.choices[0].message.content);
+    document.getElementById("notesText").innerText += completion.choices[0].message.content + "\n\n";
+    document.getElementById("notesTextDiv").scrollTo(0, document.getElementById("notesText").scrollHeight);
     this.elements = [];
   }
 }
@@ -80,16 +82,24 @@ const queue = new Queue();
 function start() {
   if(start1) { 
     document.getElementById("rawText").innerText += '\n---------- Recording Started. ----------'
+    document.getElementById('voiceButton').style.backgroundColor = "red";
+    document.getElementById('voiceButton').textContent = "Stop Recording!";
+    alert("RECORDING STARTED! NOW LOGGING TRANSCRIPT FOR NOTES TAKING!")
     recognition.start();
   }
-  else {
+  else if (!start1) {
     recognition.stop();
+    document.getElementById('voiceButton').style.backgroundColor = "#072345";
+    document.getElementById('voiceButton').textContent = "Start Recording!";
+    alert("RECORDING STOPPED! CHECK OUT YOUR NOTES BELOW!")
   }
   start1 = !start1;
 }
 
 document.getElementById('voiceButton').addEventListener('click', start);
-
+document.getElementById('deleteButtonRaw').addEventListener('click', () => clearBox("rawText"));
+document.getElementById('deleteButtonNotes').addEventListener('click', () => clearBox("notesText"));
+document.getElementById('printButton').addEventListener('click', () => printNotes());
 function clearBox(boxtype) {
   if (boxtype === "rawText") {
     console.log("raw");
@@ -101,18 +111,32 @@ function clearBox(boxtype) {
     document.getElementById("notesText").textContent = '';
   }
 }
+function printNotes() {
+  var winPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+  winPrint.document.write(document.getElementById("notesText").innerHTML);
+  winPrint.document.close();
+  winPrint.focus();
+  winPrint.print();
+  winPrint.close();
 
+}
+function putToText() {
+  document.getElementById("rawText").innerHTML = g.join("\n");
+  document.getElementById("rawTextDiv").scrollTo(0, document.getElementById("rawText").scrollHeight);
+}
 recognition.onresult = event => {
     const result = event.results[event.results.length - 1];
     if(result.isFinal) {
       g.push(result[0].transcript);
-      console.log(g);
+      console.log(queue.getQueue());
       document.getElementById("rawText").innerText += "\n\n" + g[index].toString();
+      document.getElementById("rawTextDiv").scrollTo(0, document.getElementById("rawText").scrollHeight);
       index += 1;
       if (queue.checkFull()) {
         console.log("full");
         queue.response();
       }
+
       queue.enqueue(result[0].transcript);
     }
 };
@@ -120,6 +144,7 @@ recognition.onresult = event => {
 recognition.onend = () => {
   g = [];
   index = 0;
+  queue.response();
   document.getElementById("rawText").innerText += "\n\n" + '---------- Recording Stopped. ----------';
   console.log('finished')
 };
